311dc4000272f6c94381594b1729bed57b34e6e0
cesarpoumian <cesar.poumian@nagarro.com>
2025-01-09 15:32:08 -0700

    Assets & Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
-const Home = () => <h1 className="h1-bold">Welcome to Next.js</h1>;
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Welcome to Next.js</h1>
+    <h1 className="h1-bold font-space-grotesk">Welcome to Next.js</h1>
+  </>
+);
 
 export default Home;
