635288971bd1806992484fcbe4d71b5aa00e95aa
cesarpoumian <cesar.poumian@nagarro.com>
2025-01-09 13:03:16 -0700

    Eslint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => (
-  <h1 className="text-3xl text-violet-700 font-bold">Welcome to Next.js</h1>
+  <h1 className="text-3xl font-bold text-violet-700">Welcome to Next.js</h1>
 );
 
 export default Home;
